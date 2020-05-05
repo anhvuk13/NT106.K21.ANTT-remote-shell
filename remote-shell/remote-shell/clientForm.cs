e5cfdc955bed054e178f69efd38836a88dd9aeed
anhvuk13 <anvut20@gmail.com>
2020-05-05 21:11:03 +0700

    TCP communicate

---
+++ remote-shell/remote-shell/clientForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace remote_shell
+{
+    public partial class clientForm : Form
+    {
+        private TcpClient clientSocket = null;
+        
+        public clientForm()
+        {
+            InitializeComponent();
+            clientSocket = new TcpClient();
+            clientSocket.Connect(IPAddress.Parse("127.0.0.1"), 8080); 
+        }
+
+        private void request(string command)
+        {
+            rtxbShellOutput.Text += $"❯ {command}\n";
+            NetworkStream stream = clientSocket.GetStream();
+            byte[] buffer = Encoding.UTF8.GetBytes(command);
+            stream.Write(buffer, 0, buffer.Length);
+
+            buffer = new byte[1024];
+            int byteCount = stream.Read(buffer, 0, buffer.Length);
+            string output = Encoding.UTF8.GetString(buffer, 0, byteCount);
+            if (byteCount > 0) rtxbShellOutput.Text += $"{output}\n";
+
+            txbShellCommand.Clear();
+        }
+
+        private void txbShellCommand_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (Char)Keys.Enter)
+            {
+                string command = txbShellCommand.Text;
+                switch (command)
+                {
+                    case "clear":
+                    case "cls":
+                        rtxbShellOutput.Clear();
+                        break;
+                    default:
+                        request(command);
+                        break;
+                }
+                txbShellCommand.Clear();
+            }
+        }
+    }
+}
---
+++ remote-shell/remote-shell/serverForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace remote_shell
+{
+    public partial class serverForm : Form
+    {
+        private Thread tcpServerThread = null;
+        private TcpListener serverSocket = null;
+        
+        public serverForm()
+        {
+            InitializeComponent();
+            tcpServerThread = new Thread(serverThread);
+            tcpServerThread.Start();
+        }
+
+        private void serverThread()
+        {
+            serverSocket = new TcpListener(IPAddress.Any, 8080);
+            serverSocket.Start();
+            TcpClient clientSocket = serverSocket.AcceptTcpClient();
+
+            while (Thread.CurrentThread.IsAlive && clientSocket.Connected)
+            {
+                string text = "";
+
+                NetworkStream stream = clientSocket.GetStream();
+                byte[] buffer = new byte[1024];
+                int byte_count = stream.Read(buffer, 0, buffer.Length);
+
+                if (byte_count == 0) break;
+
+                string data = Encoding.UTF8.GetString(buffer, 0, byte_count);
+
+                buffer = Encoding.UTF8.GetBytes(data);
+                stream.Write(buffer, 0, buffer.Length);
+            }
+            clientSocket.Close();
+            serverSocket.Stop();
+        }
+    }
+}
